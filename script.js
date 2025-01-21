af5834096851c8767d12d67e3ad5ca3bd46d54a4
SubhanAkhtar <superiorcollege740@gmail.com>
2025-01-21 15:31:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ if ("serviceWorker" in navigator) {
           body :'🌍Discover the beauty and wisdom of Islamic teachings.',
           icon : 'mosque.png',
         };
-        swReg.showNotification('Islam Life' , options)
+        swReg.showNotification('🌜Islam Life🌛' , options)
       })
   }
  
