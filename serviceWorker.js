2637fbbe21b4e15acfdc9f7e6860520f18f900c4
SubhanAkhtar <superiorcollege740@gmail.com>
2025-01-21 14:59:10 -0800

    add

--- serviceWorker.js
+++ serviceWorker.js
@@ -13,9 +13,7 @@ const assets = [
   "/hadees.js",
   "/Muhammadnames.js",
   "/Allahnames.js",
-  "/android/android-launchericon-48x48.png",
-  "/android/android-launchericon-72x72.png",
-  "/android/android-launchericon-96x96.png",
+  
   "/android/android-launchericon-144x144.png",
   "/android/android-launchericon-192x192.png",
   "/android/android-launchericon-512x512.png",
